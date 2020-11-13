7c23da90df0eff2d043c213514e85c2121ddbdf8
Joel-Chambergo <chambergo.joel@iisgalvanimi.edu.it>
2020-11-13 12:17:41 +0000

    punt2

--- server-node/app.js
+++ server-node/app.js
@@ -1,2 +1,15 @@
   
 
+var express = require('express');
+var app = express(); 
+const path = require('path');
+
+
+app.get('/api/index', function(req,res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+ 
+});
+
+app.listen(3000, function () {
+     console.log('Example app listening on port 3000!');
+});
