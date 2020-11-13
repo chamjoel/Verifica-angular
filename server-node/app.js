56f48827541c042bdd28a9d430e7945ba7ffd13b
Joel-Chambergo <chambergo.joel@iisgalvanimi.edu.it>
2020-11-13 12:48:16 +0000

    punt5

--- server-node/app.js
+++ server-node/app.js
@@ -14,7 +14,9 @@ app.get('/api/lotta', function(req,res) {
   res.sendFile(path.join(__dirname, 'public/lotta.html'));
  
 });
-
+app.get('/api/team',function(req,res){
+ res.sendFile(path.join(__dirname,'team.json'));
+});
 app.listen(3000, function () {
      console.log('Example app listening on port 3000!');
 });
